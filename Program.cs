94122e6d9af80a6053b97d93fee74fd1f377aa5f
hershell10 <hershellsousa@gmail.com>
2024-03-26 01:12:50 -0300

    teste verificacao fibonacci testado e aprovado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+   Console.Write("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        var pertence = VerificarFibonacci(numero);
+
+        if (pertence)
+            Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+        else
+            Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+    
+
+    static bool VerificarFibonacci(int num)
+    {
+        int a = 0;
+        int b = 1;
+
+        while (b < num)
+        {
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return b == num;
+    }
